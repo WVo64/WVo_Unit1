b58de0f2d9ef2546861df0abf5b78f8c8b20f206
Wilson Vo <brawilson@hotmail.com>
2021-02-14 11:47:49 -0800

    Finished Lesson B

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.deltaTime);
+
+
+        //Translate Method --> Vector3.forward --> Time.deltaTime
+
+        // Vector3.foward -- > (0, 0, 1)
+        // (0, 0, 1) * Time.deltaTime - (0, 0 , .016)
+        transform.Translate(Vector3.forward * Time.deltaTime * 15);
+    }
+}
